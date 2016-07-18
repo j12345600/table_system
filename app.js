42e05ff0b5c98e9d1241f31e55fdea0130682bb6
Edison Huang <a123j2010@hotmail.com>
2016-07-18 17:07:50 +0800

    add bootsrap

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var jwt = require('jsonwebtoken');
 var socketioJwt = require('socketio-jwt');
 var condA,condB,condC;
 var numOnline=0;
+var expireInMinute=24*60;
 var loginData={
 	aaa:"AAA",
 	bbb:"BBB",
@@ -21,13 +22,14 @@ var loginData={
 // var mongojs = require('mongojs');
 // var db = mongojs('contactlist', ['contactlist']);
 //var bodyParser = require('body-parser');
+//app.set('env', 'production');
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
   secret: secret.sessionSecret,
   cookie: {
-		maxAge: 24*60*60*1000,
+		maxAge: expireInMinute*60*1000,
     httpOnly: true
 		//domain: 'table.mgr.ddns.net'
 	},
@@ -47,7 +49,7 @@ app.post('/api/login', function (req, res,next) {
 		var profile = {
 	    account: req.body.account
   	};
-  	var token = jwt.sign(profile, secret.sessionSecret, { expiresInMinutes: 60*5 });
+  	var token = jwt.sign(profile, secret.sessionSecret, { expiresInMinutes: expireInMinute });
 		req.session.token=token;
 		res.type("json");
 		res.json({success:"true",token:token});
@@ -58,6 +60,30 @@ app.post('/api/login', function (req, res,next) {
 	}
 });
 app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+//
+// // production error handler
+// // no stacktraces leaked to user
+else{
+		app.use(function(err, req, res, next) {
+	    res.status(err.status || 500);
+	    res.send('<H1 style=margin-left:50%>404 Not Found</H1>');
+	});
+}
 //app.use(bodyParser.json());
 
 io.set('authorization', socketioJwt.authorize({
@@ -65,6 +91,7 @@ io.set('authorization', socketioJwt.authorize({
   handshake: true
 }));
 io.on('connection', function(socket){
+	console.log(socket.handshake);
 	//console.log(socket.handshake.decoded_token.account, 'connected');
 	console.log("sign in");
 	socket.on('disconnect',function(socket){
